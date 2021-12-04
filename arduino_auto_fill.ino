00a20bd239b940a56775691e5e8709ac032987ff
Dirk Luberth Dijkman <45427770+ldijkman@users.noreply.github.com>
2021-12-04 13:02:27 +0100

    Create arduino_auto_fill.ino

---
+++ arduino_auto_fill.ino
@@ -0,0 +1,18 @@
+Arduino_emmer_vuller_bucket_filling
+Aalsmeer emmer vuller bloemen container vuller
+
+bloemen emmer vuller Fustenvuller QuickFill Emmervuller Rackinsterter volautomatisch emmervuller
+
+Arduino flower bucket filling / auomatic bucket filling / auomatic container filling
+
+arduino bucket filling cup filling
+
+arduino i2c lcd rotary encoder for setting fill time seconds/miliseconds
+
+water valve with flat hose
+
+conveyer or lift type up down max
+
+optical sensors or better sonar no droplets errrors
+
+https://www.google.com/search?q=bloemen+emmer+vuller&hl=en&tbm=vid
